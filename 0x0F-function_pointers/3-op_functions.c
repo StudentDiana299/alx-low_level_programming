69aacff63fdb5191ecd1c90840fce4a55385d3ca
StudentDiana299 <muenidaina19@gmail.com>
2023-11-09 23:09:42 -0800

    corrections

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,7 +12,7 @@
 */
 int op_add(int a, int b)
 {
-   return (a + b);
+	return (a + b);
 }
 
 
@@ -25,7 +25,7 @@ int op_add(int a, int b)
 */
 int op_sub(int a, int b)
 {
-   return (a - b);
+	return (a - b);
 }
 
 
@@ -38,7 +38,7 @@ int op_sub(int a, int b)
 */
 int op_mul(int a, int b)
 {
-   return (a * b);
+	return (a * b);
 }
 
 
@@ -51,13 +51,11 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
-   if (b == 0)
+	if (b == 0)
    {
-       printf("Error\n");
-       exit(100);
+	printf("Error\n");
+	exit(100);
    }
-
-
    return (a / b);
 }
 
@@ -71,13 +69,11 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-   if (b == 0)
-   {
-       printf("Error\n");
-       exit(100);
-   }
-
-
-   return (a % b);
+	if (b == 0)
+	{
+	   printf("Error\n");
+	exit(100);
+	}
+	return (a % b);
 }
 
